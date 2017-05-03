d471bf7ceae70a794fd77d5c34e4e785c0b27748
JunyiXie <414623282@qq.com>
2017-05-03 12:34:01 +0800

    no message

--- Example/RecordLife/Classes/Vender/XJYChart/XJYCycleView.h
+++ Example/RecordLife/Classes/Vender/XJYChart/XJYCycleView.h
@@ -11,8 +11,7 @@
 @protocol XJYCycleViewDelegate <NSObject>
 
 /**
- 当前进度比例
-
+ ratio
  @param ratio 0.0 ~ 1.0
  */
 - (void)ratioChange:(CGFloat)ratio;
@@ -20,14 +19,9 @@
 @end
 
 @interface XJYCycleView : UIControl
-
-
 @property (nonatomic, assign) CGFloat progress;
-
 @property (nonatomic, strong) UILabel *label;
 @property (nonatomic,assign) int lineWidth;
 @property (nonatomic,setter=changeAngle:) int angle;
-//- (void)drawProgress:(CGFloat )progress;
 @property (nonatomic, weak) id<XJYCycleViewDelegate> cycleViewDeleagte;
-
 @end
--- Example/RecordLife/Classes/Vender/XJYChart/XJYCycleView.m
+++ Example/RecordLife/Classes/Vender/XJYChart/XJYCycleView.m
@@ -8,13 +8,11 @@
 
 #import "XJYCycleView.h"
 
+
+
 #define RADIANS_TO_DEGREES(radians) ((radians) * (180.0 / M_PI))
 #define DEGREES_TO_RADIANS(angle) ((angle) / 180.0 * M_PI)
-
-#define ToRad(deg) 		( (M_PI * (deg)) / 180.0 )
-#define ToDeg(rad)		( (180.0 * (rad)) / M_PI )
 #define SQR(x)			( (x) * (x) )
-
 
 @interface XJYCycleView ()
 
@@ -49,10 +47,8 @@
     [self setNeedsDisplay];
 }
 
-
 - (void)drawRect:(CGRect)rect {
-    
-    // 去除上次绘制的图层
+    // remove layer
     if (_progressLayer != nil) {
         [self.progressLayer removeFromSuperlayer];
     }
@@ -62,13 +58,11 @@
     if (_hLayer != nil) {
         [self.hLayer removeFromSuperlayer];
     }
-    
-    // 进度条的center
+    // center
     CGPoint center = CGPointMake(rect.size.width/2, rect.size.height/2);
-    CGFloat startA =  M_PI_2 +0.065;  //设置进度条起点位置
-    CGFloat endA = M_PI_2 + M_PI*2*_progress +0.065;  //设置进度条终点位置
-    
-    // 外围装饰环
+    CGFloat startA =  M_PI_2 +0.065;
+    CGFloat endA = M_PI_2 + M_PI*2*_progress +0.065;
+    // decorative cycle
     for (int i = 0; i < 180; i ++) {
         CGFloat angle = DEGREES_TO_RADIANS(i*2);
         CGFloat radiusLineCycle = rect.size.width/2 - 10;
@@ -76,18 +70,16 @@
         CGPoint point2 = CGPointMake(cos(angle)*7 + point1.x, sin(angle)*7 + point1.y);
         CGContextRef context = UIGraphicsGetCurrentContext();
         CGContextSetLineCap(context, kCGLineCapRound);
-        CGContextSetLineWidth(context, 1);  //线宽
+        CGContextSetLineWidth(context, 1);
         CGContextSetAllowsAntialiasing(context, true);
-        CGContextSetRGBStrokeColor(context, 180 / 255.0, 180.0 / 255.0, 180.0 / 255.0, 1.0);  //线的颜色
+        CGContextSetRGBStrokeColor(context, 180 / 255.0, 180.0 / 255.0, 180.0 / 255.0, 1.0);
         CGContextBeginPath(context);
-        CGContextMoveToPoint(context, point1.x, point1.y);  //起点坐标
-        CGContextAddLineToPoint(context, point2.x, point2.y);   //终点坐标
+        CGContextMoveToPoint(context, point1.x, point1.y);
+        CGContextAddLineToPoint(context, point2.x, point2.y);
         CGContextStrokePath(context);
     }
-    
-
-    // 绘制环形
-    _progressLayer = [CAShapeLayer layer];//创建一个track shape layer
+    // draw cycle
+    _progressLayer = [CAShapeLayer layer];
     _progressLayer.frame = self.bounds;
     _progressLayer.fillColor = [[UIColor clearColor] CGColor];
     // The color used to stroke the shape’s path. Animatable.
@@ -97,46 +89,38 @@
     UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:center radius:radius startAngle:startA endAngle:endA clockwise:YES];
     _progressLayer.path =[path CGPath];
     [self.layer addSublayer:_progressLayer];
-    
-    //生成渐变色
+    //gradientLayer
     self.gradientLayer = [CALayer layer];
-    
-    //左侧渐变色
+    //left gradient
     CAGradientLayer *leftLayer = [CAGradientLayer layer];
-    leftLayer.frame = CGRectMake(0, 0, self.bounds.size.width / 2, self.bounds.size.height);    // 分段设置渐变色
+    leftLayer.frame = CGRectMake(0, 0, self.bounds.size.width / 2, self.bounds.size.height);
     leftLayer.locations = @[@0.3, @0.9, @1];
     leftLayer.colors = @[(id)[UIColor colorWithRed:123/255.0 green:138/255.0 blue:146/255.0 alpha:1].CGColor,(id)[UIColor colorWithRed:178/255.0 green:187/255.0 blue:192/255.0 alpha:1].CGColor];
     [self.gradientLayer addSublayer:leftLayer];
-    
-    //右侧渐变色
+    //right gradient
     CAGradientLayer *rightLayer = [CAGradientLayer layer];
     rightLayer.frame = CGRectMake(self.bounds.size.width / 2, 0, self.bounds.size.width / 2, self.bounds.size.height);
     rightLayer.locations = @[@0.3, @0.9, @1];
     rightLayer.colors = @[(id)[UIColor colorWithRed:123/255.0 green:138/255.0 blue:146/255.0 alpha:1].CGColor,(id)[UIColor colorWithRed:64/255.0 green:89/255.0 blue:100/255.0 alpha:1].CGColor];
     [self.gradientLayer addSublayer:rightLayer];
-    [self.gradientLayer setMask:_progressLayer]; //用progressLayer来截取渐变层
+    [self.gradientLayer setMask:_progressLayer];
     [self.layer addSublayer:self.gradientLayer];
-    
-    //绘制拖动小块
+    //draw head
     CGPoint handleCenter =  [self pointFromAngle: (self.angle)];
     self.hLayer = [CAShapeLayer layer];
-    UIBezierPath *hpath = [UIBezierPath bezierPathWithArcCenter:handleCenter radius:_lineWidth*3.2 startAngle:0 endAngle:M_PI*2 clockwise:YES];//上面
+    UIBezierPath *hpath = [UIBezierPath bezierPathWithArcCenter:handleCenter radius:_lineWidth*3.2 startAngle:0 endAngle:M_PI*2 clockwise:YES];
     self.hLayer.path = hpath.CGPath;
     self.hLayer.fillColor = [UIColor colorWithRed:64/255.0 green:89/255.0 blue:100/255.0 alpha:1].CGColor;
     [self.layer addSublayer:self.hLayer];
-    
 }
 
-
-#pragma mark Touch
-
--(BOOL)beginTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event{
+#pragma mark Touch Deal
+-(BOOL)beginTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event {
     [super beginTrackingWithTouch:touch withEvent:event];
     return YES;
 }
 
-
--(BOOL)continueTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event{
+-(BOOL)continueTrackingWithTouch:(UITouch *)touch withEvent:(UIEvent *)event {
     [super continueTrackingWithTouch:touch withEvent:event];
     CGPoint lastPoint = [touch locationInView:self];
     [self movehandle:lastPoint];
@@ -144,18 +128,17 @@
     return YES;
 }
 
-
 -(void)movehandle:(CGPoint)lastPoint{
 
-    //获得中心点
+    //centerPoint
     CGPoint centerPoint = CGPointMake(self.frame.size.width/2,
                                       self.frame.size.height/2);
-    //计算中心点到任意点的角度
+    //center to point angle
     float currentAngle = AngleFromNorth(centerPoint,
                                         lastPoint,
                                         NO);
     int angleInt = floor(currentAngle);
-    //因为改变了起始点,要做些判断
+    //change the starting point so do judge
     self.angle = angleInt;
     if (self.angle >= 90 && self.angle <= 360 ) {
         self.progress = (self.angle - 90)/360.0;
@@ -166,25 +149,23 @@
     [self setNeedsDisplay];
 }
 
-//计算中心点到任意点的角度
+// computing center to the Angle of the arbitrary point
 static inline float AngleFromNorth(CGPoint p1, CGPoint p2, BOOL flipped) {
     CGPoint v = CGPointMake(p2.x-p1.x,p2.y-p1.y);
     float vmag = sqrt(SQR(v.x) + SQR(v.y)), result = 0;
     v.x /= vmag;
     v.y /= vmag;
     double radians = atan2(v.y,v.x);
-    result = ToDeg(radians);
+    result = RADIANS_TO_DEGREES(radians);
     return (result >=0  ? result : result + 360.0);
 }
 
-
 -(CGPoint)pointFromAngle:(int)angleInt{
     CGPoint centerPoint = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
-    //根据角度得到圆环上的坐标
     CGPoint result;
-    //加上了前置的量(处理渐变颜色的问题)
-    result.y = round(centerPoint.y + radius * sin(ToRad(angleInt+3))) ;
-    result.x = round(centerPoint.x + radius * cos(ToRad(angleInt+3)));
+    //Plus the amount of the front (deal with the issue of gradual change color)
+    result.y = round(centerPoint.y + radius * sin(RADIANS_TO_DEGREES(angleInt+3))) ;
+    result.x = round(centerPoint.x + radius * cos(RADIANS_TO_DEGREES(angleInt+3)));
     return result;
 }
 
