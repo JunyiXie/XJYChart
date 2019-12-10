2242f74f70f97b71d186905a2c5470dfa86176b3
Yangtsing098 <zyang.sir@gmail.com>
2019-12-10 14:24:30 +0800

    [XJYChart][M][ZyangSir]1. areaLine类型的图表增加数据线条描边，并且可以自定义线条粗细、颜色，使得数据与Y轴刻度更好进行参照，更清晰 2. 修改areaLine类图表初始化逻辑，换了一种更好看的渐变色 3. 部分方法注释完善

--- XJYChart/Configuration/LineChartConfiguration/XAreaLineChartConfiguration.h
+++ XJYChart/Configuration/LineChartConfiguration/XAreaLineChartConfiguration.h
@@ -13,6 +13,12 @@
 /// default is @[(__bridge id)[UIColor steelBlueColor].CGColor,(__bridge id)[UIColor whiteColor].CGColor];
 @property(nonatomic, strong) NSArray* gradientColors;
 
+/// 折线/曲线 线条本身的颜色
+@property(nonatomic, strong) UIColor *lineColor;
+
+/// 折线/曲线 线条本身的宽度
+@property(nonatomic, assign) CGFloat lineWidth;
+
 /// defalut is YES
 @property(nonatomic, assign) BOOL isShowPoint;
 
